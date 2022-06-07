7b48479e9e43f4352628d1e621cc19cfeb6e920e
Nobir <nobirfreelancer@gmail.com>
2022-06-07 11:36:19 +0600

    Lab 3 Task 1 Assignment

--- Mid/Lab3/Task1/resources/views/product/single.blade.php
+++ Mid/Lab3/Task1/resources/views/product/single.blade.php
@@ -2,8 +2,8 @@
 @section('contents')
     <h1>Product </h1>
     <div>
-        <strong>Product ID: </strong><span>{{$product['product_id']}}</span><br>
-        <strong>Product Name: </strong><span>{{$product["product_name"]}}</span><br>
-        <strong>Product Price: </strong><span>{{$product["product_price"]}}</span><br>
+        <strong>Product ID: </strong><span>{{ $product->product_id }}</span><br>
+        <strong>Product Name: </strong><span>{{ $product->product_name }}</span><br>
+        <strong>Product Price: </strong><span>{{ $product->product_price }}</span><br>
     </div>
 @endsection
